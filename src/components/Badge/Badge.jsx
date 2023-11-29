2d1382318fd6534a52e55cb9f34694bab85d91ae
Michelle Brown <m.andreabr@gmail.com>
2023-11-29 13:11:29 +1100

    create badge component

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-export default function Badge() {
-    
+export default function Badge( {children, ...rest} ) {
+
+    let allClasses = ['badge'];
+    rest.shape && allClasses.push(rest.shape);
+    rest.color && allClasses.push(rest.color);
+
     return (
-        <div>Badge go here!</div>
+        <div className={allClasses.join(' ')}>
+            {children}
+        </div>
     )
 }
